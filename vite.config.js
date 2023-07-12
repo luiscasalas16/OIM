33429740278e2eaf53ddf4252a53db79d81a3002
Luis Carlos Salas Villalobos <lusalas16@gmail.com>
2023-07-12 17:01:51 -0600

    Se publica la aplicación en GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  //Cambio de url base a 'OIM', para compatibilidad con github docs.
+  base: "/OIM/",
+  build: {
+    //Cambio de carpeta de output del build a 'docs', para compatibilidad con github docs.
+    outDir: "docs",
+  },
+});
